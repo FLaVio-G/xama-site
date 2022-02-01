5be2b5328d49cf03eaa025c1619050da98d34d10
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-02-01 14:48:16 -0300

    criação dos estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 import Home from "./pages/Home"
 import Header from './components/Header';
+import { GlobalStyle } from "./styles/global";
 
 
 function App(){
@@ -12,6 +13,7 @@ function App(){
      <Routes>
        <Route path = "" element = {<Home/>} />
      </Routes>
+     <GlobalStyle/>
 
    </Router>
   );
