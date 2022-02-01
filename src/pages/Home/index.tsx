8666c0225978c8838ae07321fc9d7524c4a36b3d
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-01-31 22:52:59 -0300

    criação da pages/home, configurações iniciais

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,64 @@
-import {Container} from './styles'
+import vegamsi64 from '../../assets/vegamsi64.jpg';
+import { ProductList  } from './styles';
 
 
-export function Home(){
-    return ( 
-      <Container>
-       <h1>pagina inicial</h1>
-      </Container>
-    );
-  };
-  
-  export default Home;
+export function Home() {
+  return (
+    <ProductList >
+      <li>
+        <img src={vegamsi64} alt="vegamsi64" />
+        <strong>RX vega 64</strong>
+        <button
+          type="button"
+        ></button>
+
+
+        <div>
+          Adicionar ao carrinho
+        </div>
+
+        <span> ADICIONAR AO CARRINHO</span>
+
+        <button />
+      </li>
+
+      <li>
+        <img src={vegamsi64} alt="vegamsi64" />
+        <strong>RX vega 64</strong>
+        <button
+          type="button"
+        ></button>
+
+
+        <div>
+          Adicionar ao carrinho
+        </div>
+
+        <span> ADICIONAR AO CARRINHO</span>
+
+        <button />
+      </li>
+      <li>
+        <img src={vegamsi64} alt="vegamsi64" />
+        <strong>RX vega 64</strong>
+        <button
+          type="button"
+        ></button>
+
+
+        <div>
+          Adicionar ao carrinho
+        </div>
+
+        <span> ADICIONAR AO CARRINHO</span>
+
+        <button />
+      </li>
+
+      
+    </ProductList >
+
+  );
+};
+
+export default Home;
